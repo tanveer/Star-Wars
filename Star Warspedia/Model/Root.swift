0d9d144f091c646e5952c20892f7521314c03585
Tanveer Bashir <tan.m.bashir@gmail.com>
2017-06-17 21:55:08 -0400

    Initial

---
+++ Star Warspedia/Model/Root.swift
@@ -0,0 +1,19 @@
+//
+//  Root.swift
+//  Star Warspedia
+//
+//  Created by Tanveer Bashir on 6/17/17.
+//  Copyright © 2017 Tanveer Bashir. All rights reserved.
+//
+
+import Foundation
+
+
+struct Root: Codable {
+    let films: String
+    let people: String
+    let planets: String
+    let species: String
+    let starships: String
+    let vehicles: String
+}
